f7831a61aa53c4112b12464f982bb2a161e4ac65
Mario Flores <mario.flores2@upr.edu>
2021-03-15 08:28:48 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,16 +2,14 @@ import express from 'express';
 import  BaseRouter  from './routes/BaseRouter'
 import dbCongifg from './config/db.congifg';
 import { BaseController } from './controllers/BaseController';
-// rest of the code remains same
+
 const app = express();
 const PORT = 4000;
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.get('/demo', (req, res) => res.send('Express + TypeScript Server'));
 app.use('/', new BaseRouter(new BaseController()).getRouter());
-// let pgp = require("pg-promise");
-// let client = pgp()(dbCongifg.URI);
-// client.any("select * from members");
 
 
 app.listen(PORT, () => {
